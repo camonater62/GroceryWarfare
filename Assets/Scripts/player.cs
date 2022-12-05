168b320be728f9e959a53cf3d8ab51a4aca42e29
NicolasG12 <naguevar@ucsc.edu>
2022-12-05 12:33:05 -0800

    Shits comming together

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -50,12 +50,12 @@ public class player : MonoBehaviour
         Vector3 movement = new Vector3(movementX, 0, movementZ);
         Debug.Log(movement);
         rb.AddRelativeForce(movement * speed); //Local space
-         //rb.AddForce(movement * speed); //World space
-        // transform.Translate(new Vector3(-movementY, 0.0f, movementX) * speed * Time.fixedDeltaTime);
+                                               //rb.AddForce(movement * speed); //World space
+                                               // transform.Translate(new Vector3(-movementY, 0.0f, movementX) * speed * Time.fixedDeltaTime);
 
-        //Vector3 rotation = new Vector3(-rotationY, rotationX, 0);
-        //Quaternion deltaRotation = Quaternion.Euler(rotation * 10.0f * Time.fixedDeltaTime);
-        //rb.MoveRotation(rb.rotation * deltaRotation);
+        Vector3 rotation = new Vector3(0, rotationX, 0);
+        Quaternion deltaRotation = Quaternion.Euler(rotation * 10.0f * Time.fixedDeltaTime);
+        rb.MoveRotation(rb.rotation * deltaRotation);
 
     }
 }
