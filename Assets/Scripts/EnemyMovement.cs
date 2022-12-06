88b36833e5ada54148f559060d57a1a65ab771d5
NicolasG12 <naguevar@ucsc.edu>
2022-12-06 11:22:56 -0800

    Made a navmesh and character moves

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public Transform goal;
+
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+}
