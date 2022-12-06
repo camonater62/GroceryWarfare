effe11b32ac4a271ccc5fcf4e31024461c322bcd
NicolasG12 <naguevar@ucsc.edu>
2022-12-06 11:50:11 -0800

    Agent now has a patrol path

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -6,11 +6,28 @@ using UnityEngine.AI;
 public class EnemyMovement : MonoBehaviour
 {
 
-    public Transform goal;
+    public Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
 
     void Start()
     {
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
-        agent.destination = goal.position;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void GotoNextPoint()
+    {
+        if (points.Length == 0) return;
+        agent.destination = points[destPoint].position;
+
+        destPoint = (destPoint+ 1) % points.Length; 
+    }
+
+    private void Update()
+    {
+        if(!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            GotoNextPoint();
+        }
     }
 }
