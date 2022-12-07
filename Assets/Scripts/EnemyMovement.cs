44dc9af1cff0f7de9b4c1c5bf288c1f1a098a156
NicolasG12 <naguevar@ucsc.edu>
2022-12-07 11:26:49 -0800

    Added AI

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,34 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void GotoNextPoint()
+    {
+        if (points.Length == 0) return;
+        agent.destination = points[destPoint].position;
+
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+    private void Update()
+    {
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            GotoNextPoint();
+        }
+    }
+}
